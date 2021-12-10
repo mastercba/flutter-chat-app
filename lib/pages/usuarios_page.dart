aa55db8b72ad2395a4b28eddf5b15b26fc38fad3
Marco Torrico <marcojtorrico@gmail.com>
2021-12-10 17:59:50 -0400

    7.3.AppBar UsuariosPage

--- lib/pages/usuarios_page.dart
+++ lib/pages/usuarios_page.dart
@@ -1,9 +1,39 @@
 import 'package:flutter/material.dart';
 
-class UsuariosPage extends StatelessWidget {
+import 'package:chat/models/usuario.dart';
+
+class UsuariosPage extends StatefulWidget {
+  @override
+  _UsuariosPageState createState() => _UsuariosPageState();
+}
+
+class _UsuariosPageState extends State<UsuariosPage> {
+  final usuarios = [
+    Usuario(uid: '1', nombre: 'María', email: 'test1@test.com', online: true),
+    Usuario(uid: '2', nombre: 'Meli', email: 'test2@test.com', online: false),
+    Usuario(uid: '3', nombre: 'Fer', email: 'test3@test.com', online: true),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Mi Nombre', style: TextStyle(color: Colors.black87)),
+        elevation: 1,
+        backgroundColor: Colors.white,
+        leading: IconButton(
+          icon: Icon(Icons.exit_to_app, color: Colors.black87),
+          onPressed: () {},
+        ),
+        // registra el estado de la conexion
+        actions: <Widget>[
+          Container(
+            margin: EdgeInsets.only(right: 10),
+            child: Icon(Icons.check_circle, color: Colors.blue[400]),
+            // child: Icon( Icons.offline_bolt, color: Colors.red ),
+          )
+        ],
+      ),
       body: Center(
         child: Text('Usuarios Page'),
       ),
