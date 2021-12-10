58316c8c27e469bf39c158dac685672aefdcf935
Marco Torrico <marcojtorrico@gmail.com>
2021-12-10 16:23:13 -0400

    Login y Registro terminado

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,81 @@
+import 'package:chat/widgets/boton_azul.dart';
+import 'package:flutter/material.dart';
+
+import 'package:chat/widgets/labels.dart';
+import 'package:chat/widgets/logo.dart';
+import 'package:chat/widgets/custom_input.dart';
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Color(0xffF2F2F2),
+        body: SafeArea(
+          child: SingleChildScrollView(
+            //para que en ios y android parece que rebota
+            physics: BouncingScrollPhysics(),
+            child: Container(
+              //para que x el scrollview no se junten todos los childs
+              //tomamos el max de alto de pantalla  *0.9
+              height: MediaQuery.of(context).size.height * 0.9,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Logo(titulo: 'Messenger'),
+                  _Form(),
+                  Labels(
+                    ruta: 'register',
+                    titulo: '¿No tienes cuenta?',
+                    subTitulo: 'Crea una ahora!',
+                  ),
+                  Text(
+                    'Términos y condiciones de uso',
+                    style: TextStyle(fontWeight: FontWeight.w200),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ));
+  }
+}
+
+class _Form extends StatefulWidget {
+  @override
+  __FormState createState() => __FormState();
+}
+
+class __FormState extends State<_Form> {
+  final emailCtrl = TextEditingController();
+  final passCtrl = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 40),
+      padding: EdgeInsets.symmetric(horizontal: 50),
+      child: Column(
+        children: <Widget>[
+          CustomInput(
+              icon: Icons.mail_outline,
+              placeholder: 'Correo',
+              keyboardType: TextInputType.emailAddress,
+              textController: emailCtrl),
+          CustomInput(
+            icon: Icons.lock_outline,
+            placeholder: 'Contraseña',
+            textController: passCtrl,
+            isPassword: true,
+          ),
+          BotonAzul(
+            text: 'Ingrese',
+            onPressed: () {
+              print(emailCtrl.text);
+              print(passCtrl.text);
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/widgets/labels.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class Labels extends StatelessWidget {
+  final String ruta;
+  final String titulo;
+  final String subTitulo;
+
+  const Labels({
+    required this.ruta,
+    required this.titulo,
+    required this.subTitulo,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: <Widget>[
+          Text(this.titulo,
+              style: TextStyle(
+                  color: Colors.black54,
+                  fontSize: 15,
+                  fontWeight: FontWeight.w300)),
+          SizedBox(height: 10),
+          GestureDetector(
+            //me permite ponerle cualquier gesto y reconocerlo
+            child: Text(this.subTitulo,
+                style: TextStyle(
+                    color: Colors.blue[600],
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold)),
+            onTap: () {
+              Navigator.pushReplacementNamed(context, this.ruta);
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
